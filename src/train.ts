4b7956a557e98646e573144713c9b6d77485b6dd
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-10-15 02:41:20 +0900

    feat: task H2 solution

--- src/train.ts
+++ src/train.ts
@@ -1,4 +1,35 @@
+// task H2
+
+import { isNumberObject } from "util/types";
+
+function getDigits(str: string) {
+  const text = str.split("");
+  const num: number[] = [];
+  for (let i = 0; i < text.length; i++) {
+    if (!isNaN(Number(text[i]))) {
+      num.push(Number(text[i]));
+    }
+  }
+
+  console.log("raqamlar:", num.join(""));
+}
+
+getDigits("11m");
+
+
+
+
+
+
+
+
+
+
+
+
+
 //task H 1-usul
+
 // function getPositive(arr:number[]){
     
 //         const positiv = arr.filter(item=>item>0)
@@ -8,17 +39,17 @@
 // console.log(getPositive(numbers));
 
 // 2-usul
-function getPositive(arr:number[]){
-    const positivnumber: number[] = [];
-    for(let i=0; i<arr.length; i++)
-        {
-        if(arr[i]>0){
-            positivnumber.push(arr[i]);
-        }
-    }
-    return positivnumber;
-}
-const numbers = [2,3,-1.0,8,10,-3,-7]
-console.log(getPositive(numbers));
+// function getPositive(arr:number[]){
+//     const positivnumber: number[] = [];
+//     for(let i=0; i<arr.length; i++)
+//         {
+//         if(arr[i]>0){
+//             positivnumber.push(arr[i]);
+//         }
+//     }
+//     return positivnumber;
+// }
+// const numbers = [2,3,-1.0,8,10,-3,-7]
+// console.log(getPositive(numbers));
 
 
