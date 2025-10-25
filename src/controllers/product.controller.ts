6753d6ff59246bb87627b3c03a766a0175d2eeea
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-10-26 03:26:39 +0900

    feat: locals variable integration

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -10,7 +10,7 @@ const productController: T = {};
 productController.getAllProducts = async (req: Request, res: Response)=>{
     try
     {
-        console.log('getAllProducts ')
+        console.log('getAllProducts ');
         res.render("products")
     }
     catch(err){
