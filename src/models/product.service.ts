49b90f4794bf3429ba2557c058bf220f6d24f021
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-10-28 02:13:22 +0900

    fix: getAllProducts api and passing data into ejs

--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -14,6 +14,14 @@ class ProductService {
 
     /** SSR */
 
+    public async getAllProducts(): Promise<Product[]>{
+        
+        const result = await this.productModel.find().exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND)
+            console.log("result:", result)
+        return result;
+    }
+    
     public async createNewProduct(input: ProductInput): Promise<Product>{
         try{
             return await this.productModel.create(input);
@@ -29,7 +37,7 @@ class ProductService {
         id = shapeIntoMongooseObjectId(id)
         const result = await this.productModel.findByIdAndUpdate({_id: id}, input, {new: true}).exec();
         if(!result) throw new Errors(HttpCode.NOT_MODIFIED, Message.UPDATE_FAILED)
-            console.log("result:", result)
+            //console.log("result:", result)
         return result;
     }
 
