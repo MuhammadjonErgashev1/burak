96e2e7e1f252a24940cc10c90d24ccb9e629a1fc
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-11-04 08:29:48 +0900

    feat: update chosen prodect frontend logic

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -49,8 +49,8 @@ function validateSignupForm(){
         return false;
     }
 
-    const memberImage = $(".member-image")?.get[0].files[0] ? 
-    $(".member-image")?.get[0]?.files[0].name: null;
+    const memberImage = $(".member-image")?.get[0]?.files[0]?.name ? 
+    $(".member-image")?.get[0]?.files[0]?.name: null;
     if(!memberImage){
         alert("Please insert restaurant image!")
         return false;
