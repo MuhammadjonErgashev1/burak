3a5cb595e9bb002463e6d43bbc36fead9119e656
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-11-23 21:30:36 +0900

    feat: develop updateMember business logic

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,14 @@
 import memberController from './controllers/member.controller';
 
 import express from 'express';
+import uploader from './libs/utils/uploader';
 const router = express.Router();
 /* memeber */ 
 router.post('/member/login', memberController.login)
 router.post('/member/signup', memberController.signup)
 router.post("/member/logout", memberController.verifyAuth, memberController.logout)
 router.get("/member/detail", memberController.verifyAuth, memberController.getMemberDetail)
+router.post("/member/update", uploader("members").single("memberImage") , memberController.verifyAuth, memberController.updateMember)
 
 /*Product*/ 
 
