c4c127a61a374a292a0b12f5015f21ae48e9cb7a
MuhammadjonErgashev1 <mmuhammadjonergashev1@gmail.com>
2025-11-23 23:01:02 +0900

    feat: develop gettopUsers rest Api

--- src/router.ts
+++ src/router.ts
@@ -8,7 +8,10 @@ router.post('/member/login', memberController.login)
 router.post('/member/signup', memberController.signup)
 router.post("/member/logout", memberController.verifyAuth, memberController.logout)
 router.get("/member/detail", memberController.verifyAuth, memberController.getMemberDetail)
-router.post("/member/update", uploader("members").single("memberImage") , memberController.verifyAuth, memberController.updateMember)
+router.post("/member/update", 
+    memberController.verifyAuth, 
+    uploader("members").single("memberImage") ,  memberController.updateMember)
+router.get("/member/top-users", memberController.getTopUsers)
 
 /*Product*/ 
 
